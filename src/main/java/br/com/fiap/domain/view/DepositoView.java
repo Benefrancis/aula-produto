29a0b110167e3f62ad2280e65526abe06995203b
Benefrancis <benefrancis@gmail.com>
2023-08-10 11:16:56 -0300

    Implementamos o domínio Deposito.

--- src/main/java/br/com/fiap/domain/view/DepositoView.java
+++ src/main/java/br/com/fiap/domain/view/DepositoView.java
@@ -1,15 +1,21 @@
 package br.com.fiap.domain.view;
 
 import br.com.fiap.domain.entity.Deposito;
-import br.com.fiap.domain.entity.Produto;
 
 import javax.swing.*;
-import java.math.BigDecimal;
 
 public class DepositoView {
 
     public Deposito form() {
-        String nome = JOptionPane.showInputDialog("Nome do Deposito");
-        return new Deposito(nome);
+
+        String nome = null;
+
+        do {
+            nome = JOptionPane.showInputDialog( "Nome do Depósito" );
+
+        } while (nome == null);
+
+
+        return new Deposito( null, nome );
     }
 }
